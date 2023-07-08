054748d9dd29c309df5f01fead3c66ad47fa9e7d
hades102zues <joshua.kar@hotmail.com>
2023-07-08 17:08:05 -0400

    minor thoughts

--- src/game-server/game_server.h
+++ src/game-server/game_server.h
@@ -2,12 +2,15 @@
 
 #include "../network-server/server.h"
 #include "game_state.h"
+#include <unordered_map>
 
 // #include "room.h"
 
 class HiddenGameServer : public HiddenServer {
     private:
         std::vector<EntityState> m_gameState;
+        std::unordered_map<unsigned int, bool> m_players; 
+        unsigned int m_max_players;
 
     public:
         HiddenGameServer(const char* ip, int port, machine host_machine, const int maxClients);
